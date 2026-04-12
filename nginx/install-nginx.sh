4fd8d96a8bc317db756e619836b448dd0cc5e25d
gokhan-cli <gokhansezen@gmail.com>
2026-04-12 20:06:07 +0300

    Create install-nginx.sh for Nginx setup
    Add installation script for Nginx and its components.

---
+++ nginx/install-nginx.sh
@@ -0,0 +1,16 @@
+### 1. Nginx ve Gerekli Bileşenlerin Kurulumu
+
+İşletim sisteminize uygun Nginx sürümünü kurun ve kurulan sürümü kontrol edin:
+
+```bash
+sudo apt update
+sudo apt install nginx=1.29.4-1~noble curl wget unzip -y
+
+# WAF uyumluluğunun bozulmaması için Nginx sürümünü sabitliyoruz (hold):
+sudo apt-mark hold nginx
+
+nginx -v # Çıkan sürüm: nginx version: nginx/1.29.4
+
+sudo systemctl enable nginx
+sudo systemctl start nginx
+```
