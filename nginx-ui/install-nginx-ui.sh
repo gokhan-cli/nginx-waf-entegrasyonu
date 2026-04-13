e91fdd1af4e0d398dc805a7bd37ab1eb8f43cd05
gokhan-cli <gokhansezen@gmail.com>
2026-04-13 13:00:27 +0300

    Update installation instructions for NginxUI

--- nginx-ui/install-nginx-ui.sh
+++ nginx-ui/install-nginx-ui.sh
@@ -1,9 +1,7 @@
 ### 2. NginxUI (Web Panel) Kurulumu
 
-NginxUI aracını resmi betik yardımıyla sunucu servisi olarak (native) kurabilirsiniz:
+# NginxUI aracını resmi betik yardımıyla sunucu servisi olarak (native) kurabilirsiniz:
 
-```bash
 bash <(curl -L -s https://raw.githubusercontent.com/0xJacky/nginx-ui/master/install.sh) install
-```
 
-Kurulum bittikten sonra web paneline `http://<Sunucu-IP>:9000` portundan giriş yapabilirsiniz. Varsayılan kullanıcı adı/şifre sistemi `admin`/`admin` dir. Girdiğiniz gibi şifreyi değiştirin.
+# Kurulum bittikten sonra web paneline `http://<Sunucu-IP>:9000` portundan giriş yapabilirsiniz. Varsayılan kullanıcı adı/şifre sistemi `admin`/`admin` dir. Girdiğiniz gibi şifreyi değiştirin.
