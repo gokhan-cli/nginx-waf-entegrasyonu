67fd5c63160a5861cae0470d279508f0934bb25c
gokhan-cli <gokhansezen@gmail.com>
2026-04-21 10:39:48 +0300

    Update install-open-appsec.sh

--- open-appsec/install-open-appsec.sh
+++ open-appsec/install-open-appsec.sh
@@ -8,9 +8,6 @@ wget https://downloads.openappsec.io/open-appsec-install && chmod +x open-appsec
 # OpenAppSec'i yerel yönetim için download modunda çalıştıran komut:
 sudo ./open-appsec-install --download # OpenAppSec localde çalışağı için biz bu modu kullanacağız.
 
-#Eğer yukarıdaki script ile kurulumda sorun yaşarsanız, klasör içindeki dosyayı sunucunuza çekip kullanabilirsiniz.
-wget https://raw.githubusercontent.com/gokhan-cli/nginx-nginxui-waf-fail2ban-stack/refs/heads/main/open-appsec/open-appsec-install && chmod +x open-appsec-install
-
 # Yine kurulum için aşağıdaki parametreleri kullanacağız.
 sudo ./open-appsec-install --download
 
