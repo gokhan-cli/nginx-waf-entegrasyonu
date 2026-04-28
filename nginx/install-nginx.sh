f07578e8a7818988d8e82273a81bc9c932a63ecc
gokhan-cli <gokhansezen@gmail.com>
2026-04-28 08:15:27 +0300

    Delete nginx/install-nginx.sh

--- nginx/install-nginx.sh
+++
@@ -1,9 +0,0 @@
-### 1. Nginx ve Gerekli Bileşenlerin Kurulumu
-
-# İşletim sisteminize uygun Nginx sürümünü kurun ve kurulan sürümü kontrol edin:
-
-sudo apt update
-sudo apt install nginx curl wget unzip -y
-
-sudo systemctl enable nginx
-sudo systemctl start nginx
